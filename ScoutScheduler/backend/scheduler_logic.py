9079149edcbe3e09e54953b495a3167ce631f8c9
Yunus <ymarikkar.03@gmail.com>
2025-04-21 18:19:36 +0100

    working on sch logic

--- ScoutScheduler/backend/scheduler_logic.py
+++ ScoutScheduler/backend/scheduler_logic.py
@@ -1,3 +1,24 @@
-def generate_schedule():
-    # TODO: Implement intelligent scheduling logic
-    pass
+import pandas as pd
+
+business_days = pd.bdate_range(
+    start="2025-09-01",
+    end="2026-07-31",
+    freq='B',
+    holidays=school_holidays_list
+)
+
+from dateutil.rrule import rrule, DAILY
+from datetime import datetime
+
+rule = rrule(DAILY, dtstart=datetime(2025,9,1), until=datetime(2026,7,31))
+for holiday in school_holidays_list:
+    rule = rule.exdate(holiday)
+available_dates = list(rule)
+
+def get_available_dates(start, end, holidays):
+    # use pandas or dateutil as above
+    return date_list
+
+def assign_sessions(badges, available_dates, max_per_week=None):
+    # choose greedy or CP method
+    return scheduled_sessions  # list of (date, badge_name)
